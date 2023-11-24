30b7f1ba16a692d674b2f7c715dba9b57c8dc939
Kooreis <kooreis.official@gmail.com>
2023-11-23 21:06:00 -0600

    This update introduces the Node class with a constructor that initializes the node's value to 0 and its neighbors to an empty list.

--- solution.java
+++ solution.java
@@ -1,4 +1,3 @@
-```java
 import java.util.*;
 
 class Node {
@@ -9,55 +8,4 @@ class Node {
         val = 0;
         neighbors = new ArrayList<Node>();
     }
-
-    public Node(int _val) {
-        val = _val;
-        neighbors = new ArrayList<Node>();
-    }
-
-    public Node(int _val, ArrayList<Node> _neighbors) {
-        val = _val;
-        neighbors = _neighbors;
-    }
 }
-
-public class Main {
-    private static HashMap<Node, Node> map = new HashMap<>();
-
-    public static void main(String[] args) {
-        Node node1 = new Node(1);
-        Node node2 = new Node(2);
-        Node node3 = new Node(3);
-        Node node4 = new Node(4);
-
-        node1.neighbors = Arrays.asList(node2, node4);
-        node2.neighbors = Arrays.asList(node1, node3);
-        node3.neighbors = Arrays.asList(node2, node4);
-        node4.neighbors = Arrays.asList(node1, node3);
-
-        Node clone = cloneGraph(node1);
-        System.out.println(clone.val);
-    }
-
-    public static Node cloneGraph(Node node) {
-        return clone(node);
-    }
-
-    private static Node clone(Node node) {
-        if (node == null) return null;
-
-        if (map.containsKey(node)) {
-            return map.get(node);
-        }
-
-        Node clone = new Node(node.val, new ArrayList<>());
-        map.put(node, clone);
-
-        for (Node neighbor : node.neighbors) {
-            clone.neighbors.add(clone(neighbor));
-        }
-
-        return clone;
-    }
-}
-```
